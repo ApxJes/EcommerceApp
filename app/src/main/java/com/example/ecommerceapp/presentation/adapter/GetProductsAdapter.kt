9aabe8337988eecd4e39542d2908ed51c4abcc92
aungpyaekyaw <shwyzee109@gmail.com>
2025-05-02 12:06:37 +0630

    Get hot deals products

---
+++ app/src/main/java/com/example/ecommerceapp/presentation/adapter/GetProductsAdapter.kt
@@ -0,0 +1,76 @@
+package com.example.ecommerceapp.presentation.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.ecommerceapp.R
+import com.example.ecommerceapp.domain.model.Product
+
+class GetProductsAdapter : RecyclerView.Adapter<GetProductsAdapter.ProductsViewHolder>() {
+
+    private val differCallBack = object : DiffUtil.ItemCallback<Product>() {
+        override fun areItemsTheSame(
+            oldItem: Product,
+            newItem: Product
+        ): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(
+            oldItem: Product,
+            newItem: Product
+        ): Boolean {
+            return oldItem == newItem
+        }
+    }
+
+    val differ = AsyncListDiffer(this, differCallBack)
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ProductsViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_layout, parent, false)
+
+        return ProductsViewHolder(view)
+    }
+
+    override fun onBindViewHolder(
+        holder: ProductsViewHolder,
+        position: Int
+    ) {
+        val product = differ.currentList[position]
+        holder.apply {
+            Glide.with(itemView.context)
+                .load(product.thumbnail)
+                .into(productImage)
+
+            productName.text = product.title
+            productBrand.text = product.brand
+            productPrice.text = product.price.toString()
+            productRating.text =
+                product.reviews?.firstOrNull { it?.rating != null }?.rating?.toString() ?: "0.0"
+
+        }
+
+    }
+
+    override fun getItemCount(): Int {
+        return differ.currentList.size
+    }
+
+    inner class ProductsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val productImage: ImageView = itemView.findViewById(R.id.imvProductImage)
+        val productName: TextView = itemView.findViewById(R.id.txvProductName)
+        val productPrice: TextView = itemView.findViewById(R.id.txvProductPrize)
+        val productRating: TextView = itemView.findViewById(R.id.txvProductRating)
+        val productBrand: TextView = itemView.findViewById(R.id.txvProductBrand)
+    }
+}
--- app/src/main/java/com/example/ecommerceapp/presentation/ui/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/ecommerceapp/presentation/ui/fragment/HomeFragment.kt
@@ -1,19 +1,30 @@
 package com.example.ecommerceapp.presentation.ui.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.ecommerceapp.R
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.ecommerceapp.databinding.FragmentHomeBinding
+import com.example.ecommerceapp.presentation.adapter.GetProductsAdapter
+import com.example.ecommerceapp.presentation.viewMdoel.GetProductsViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
+    private lateinit var productAdpater: GetProductsAdapter
+    private val viewModel: GetProductsViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -30,6 +41,50 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        productAdpater = GetProductsAdapter()
+        getProducts()
+        setUpRecyclerViewForProducts()
+    }
+
+    private fun getProducts() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.state.collectLatest { products ->
+                    binding.loadingProgressBar.visibility =
+                        if (products.isLoading) View.VISIBLE else View.GONE
+
+                    if(products.products.isNotEmpty()) {
+                         productAdpater.differ.submitList(products.products)
+                    }
+
+                }
+            }
+        }
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewModel.getProducts()
+            viewLifecycleOwner.lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.event.collect { event ->
+                    when(event) {
+                        is GetProductsViewModel.UiEvent.ToastMessage -> {
+                            Toast.makeText(
+                                requireContext(),
+                                event.message,
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
+
+                }
+            }
+        }
+    }
+
+    private fun setUpRecyclerViewForProducts() {
+        binding.rcvHotDeals.apply {
+            adapter = productAdpater
+            layoutManager = GridLayoutManager(requireActivity(), 2)
+        }
     }
 
     override fun onDestroyView() {
