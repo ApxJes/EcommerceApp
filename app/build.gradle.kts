2668ca2b54c65dd6458d47e12ccc0047ced0ae04
aungpyaekyaw <shwyzee109@gmail.com>
2025-05-08 13:04:44 +0630

    update user's profile

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,43 +62,23 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 
-    // Hilt Dependencies
     implementation("com.google.dagger:hilt-android:2.50")
     kapt("com.google.dagger:hilt-compiler:2.50")
-
-    // Hilt with Navigation (For Fragments)
     implementation("androidx.hilt:hilt-navigation-fragment:1.0.0")
-
-    // ViewModel & LiveData
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
-
-    // Activity KTX (For ViewModel delegation)
     implementation("androidx.activity:activity-ktx:1.8.2")
-
-    // Retrofit core library
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
-
-    // Converter for JSON (Gson is commonly used)
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-
-    // Logging interceptor for debugging API requests and responses
     implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
-
-    // Room
     implementation ("androidx.room:room-runtime:2.6.0")
     kapt("androidx.room:room-compiler:2.6.0")
-
-    // Kotlin Extensions and Coroutines support for Room
     implementation ("androidx.room:room-ktx:2.6.0")
-
-    // Navigation Components
     implementation ("androidx.navigation:navigation-fragment-ktx:2.7.5")
     implementation ("androidx.navigation:navigation-ui-ktx:2.7.5")
-
     implementation ("com.github.bumptech.glide:glide:4.16.0")
     kapt ("com.github.bumptech.glide:compiler:4.16.0")
-
     implementation ("com.google.android.gms:play-services-auth:20.7.0")
     implementation ("com.github.denzcoskun:ImageSlideshow:0.1.0")
+    implementation ("de.hdodenhof:circleimageview:3.1.0")
 }
--- app/src/main/java/com/example/ecommerceapp/presentation/ui/fragment/AccountFragment.kt
+++ app/src/main/java/com/example/ecommerceapp/presentation/ui/fragment/AccountFragment.kt
@@ -1,5 +1,6 @@
 package com.example.ecommerceapp.presentation.ui.fragment
 
+import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +8,20 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.net.toUri
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavOptions
+import androidx.navigation.fragment.findNavController
+import com.example.ecommerceapp.R
 import com.example.ecommerceapp.databinding.FragmentAccountBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.UserProfileChangeRequest
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
 
 @AndroidEntryPoint
 class AccountFragment : Fragment() {
@@ -19,16 +29,21 @@ class AccountFragment : Fragment() {
     private var _binding: FragmentAccountBinding? = null
     private val binding get() = _binding!!
     private lateinit var auth: FirebaseAuth
+
     private var selectImageUri: Uri? = null
 
-    private val resultContract = registerForActivityResult(
-        ActivityResultContracts.GetContent()
-    ) { uri ->
-        uri.let {
-            selectImageUri = it
-            binding.imvProfile.setImageURI(selectImageUri)
+    private val imagePickerLauncher =
+        registerForActivityResult(ActivityResultContracts.OpenDocument()) { uri: Uri? ->
+            uri?.let {
+                requireContext().contentResolver.takePersistableUriPermission(
+                    it,
+                    Intent.FLAG_GRANT_READ_URI_PERMISSION
+                )
+                selectImageUri = it
+                binding.imvProfile.setImageURI(it)
+            }
         }
-    }
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -42,42 +57,84 @@ class AccountFragment : Fragment() {
         )
 
         return binding.root
+        requireActivity().contentResolver.persistedUriPermissions
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         auth = FirebaseAuth.getInstance()
 
-        binding.imvProfile.setOnClickListener {
-            resultContract.launch("image/*")
+        checkLoginState()
+
+        binding.imvBack.setOnClickListener {
+            findNavController().navigate(
+                R.id.homeFragment, null, NavOptions.Builder()
+                    .setPopUpTo(R.id.onBoardingFragment, true)
+                    .build()
+            )
         }
-        
+
         binding.imvSave.setOnClickListener {
+            updateProfile()
+        }
+
+        binding.imvProfile.setOnClickListener {
+            imagePickerLauncher.launch(arrayOf("image/*"))
+        }
+    }
+
+    private fun updateProfile() {
+        auth.currentUser?.let { user ->
             val userName = binding.edtName.text.toString()
             val profileUpdate = UserProfileChangeRequest.Builder()
                 .setDisplayName(userName)
-                .setPhotoUri(selectImageUri)
+                .apply {
+                    selectImageUri.let { photoUri = it }
+                }
                 .build()
 
-            auth.currentUser?.updateProfile(profileUpdate)
-                ?.addOnCompleteListener { task ->
-                    if(task.isSuccessful) {
-                        Toast.makeText(requireContext(), "Profile updated", Toast.LENGTH_SHORT).show()
-                        binding.txvName.text = userName
-                        binding.txvEmail.text = auth.currentUser?.email
-                    } else {
-                        Toast.makeText(requireContext(), "Failed to update profile", Toast.LENGTH_SHORT).show()
+            CoroutineScope(Dispatchers.IO).launch {
+                try {
+
+                    user.updateProfile(profileUpdate).await()
+                    withContext(Dispatchers.Main) {
+                        checkLoginState()
+                        Toast.makeText(
+                            requireContext(),
+                            "Successfully updated user profile",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+
+                } catch (e: Exception) {
+                    withContext(Dispatchers.Main) {
+                        Toast.makeText(
+                            requireContext(),
+                            e.message,
+                            Toast.LENGTH_SHORT
+                        ).show()
                     }
                 }
-        }
-
-        auth.currentUser?.let { user ->
-            binding.txvName.text = user.displayName
-            binding.txvEmail.text = user.email
-            user.photoUrl?.let {
-                binding.imvProfile.setImageURI(it)
             }
+        }
+    }
 
+    private fun checkLoginState() {
+        val user = auth.currentUser
+        user?.let {
+            binding.txvName.text = user.displayName ?: ""
+            binding.txvEmail.text = user.email ?: ""
+            binding.edtName.setText(user.displayName ?: "")
+
+            if (user.photoUrl != null) {
+                try {
+                    binding.imvProfile.setImageURI(user.photoUrl)
+                } catch (e: SecurityException) {
+                    binding.imvProfile.setImageResource(R.drawable.pp)
+                }
+            } else {
+                binding.imvProfile.setImageResource(R.drawable.pp)
+            }
         }
     }
 
